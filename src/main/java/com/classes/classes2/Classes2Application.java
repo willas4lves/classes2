9dbbb56197042904a7830033eb5dd5a3f7152f69
willas <nascimentowillas5@gmail.com>
2024-07-03 20:31:30 -0300

    TIVE QUE FAZER outro repositor  PQ DEU CONFLITO  no repositor classes

--- src/main/java/com/classes/classes2/Classes2Application.java
+++ src/main/java/com/classes/classes2/Classes2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Classes2Application {
+	//TIVE QUE REFAZER TUDO PORQUE DEU CONFLITO NOS ARQUIVOS.
 
 	public static void main(String[] args) {
 		SpringApplication.run(Classes2Application.class, args);
